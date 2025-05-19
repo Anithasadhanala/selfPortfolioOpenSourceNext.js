2aba2158471a36d77baa72bbd761e01b89d5df48
Anitha Sai <84316640+Anithasadhanala@users.noreply.github.com>
2025-05-19 17:09:03 +0530

    Update selfDetails.js

--- data/selfDetails.js
+++ data/selfDetails.js
@@ -16,6 +16,6 @@ export const selfDetails = [
         name: "ANITHA",
         jobRole: "WEB DEVELOPER",
         description: "I’m developer and problem solver from India with high passion of creating softwares That looks and works great. I am passionate about the solving the real world problems ensuring all the possible testcases.",
-        resumeDriveLink: "https://drive.google.com/file/d/1ydMp7YVfmzL74IUUEko3tRRDh_LiYAA_/view?usp=sharing",
+        resumeDriveLink: "https://drive.google.com/file/d/1RFrlfdvNZ9MmO5MsugI4u9Iwq1P5PkRg/view?usp=sharing",
     }
 ]
